142e01b9760849971e9912d0193611a6d3639d99
IsaacG185 <gisaac@wustl.edu>
2025-09-27 23:11:22 -0500

    Delete gisaacNGC2992.sh

--- gisaacNGC2992.sh
+++
@@ -1,6 +0,0 @@
-source /faculty/mnowak/.load_xspec_bash_34 # ADJUST
-export CALDB=/man1/CALDB # ADJUST
-echo " CALDB is set to: $CALDB"
-export CALDBINIT=/man1/CALDB/software/tools/caldbinit.sh # ADJUST
-echo " CALDBINIT is set to: $CALDBINIT"
-isis -g /ugrad/gisaac/Downloads/NGC_2992/Scripts/project_code.isis # ADJUST
